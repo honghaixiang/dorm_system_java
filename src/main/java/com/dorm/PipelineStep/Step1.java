89768418fec83a320f196142a15edd223177fc64
honghaixiang <1973503594@qq.com>
2026-03-06 11:58:50 +0800

    第二次提交

--- src/main/java/com/dorm/PipelineStep/Step1.java
+++ src/main/java/com/dorm/PipelineStep/Step1.java
@@ -1,12 +1,8 @@
 package com.dorm.PipelineStep;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Step1 implements PipelineStep {
     @Override
-    public void run(String data) {
-        System.out.println("【步骤1】处理数据: " + data);
-        // 你的业务逻辑
+    public void run() {
+        System.out.println("执行 Step1");
     }
 }
--- src/main/java/com/dorm/PipelineStep/Step2.java
+++ src/main/java/com/dorm/PipelineStep/Step2.java
@@ -1,12 +1,8 @@
 package com.dorm.PipelineStep;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Step2 implements PipelineStep {
     @Override
-    public void run(String data) {
-        System.out.println("【步骤2】处理数据: " + data);
-        // 你的业务逻辑
+    public void run() {
+        System.out.println("执行 Step2");
     }
 }
